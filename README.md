443316fa42f797418087864090a97ffaabe422ce
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-11-26 02:08:10 +0600

    all fill server gi tadd and teest

---
+++ README.md
@@ -0,0 +1,2 @@
+"# b612-used-products-resale-server-side-skmamunkhan072" 
+"# b612-used-products-resale-server-side-skmamunkhan072" 
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("laptops second hand products server  is running");
+});
+
+app.listen(port, () =>
+  console.log(`laptops second hand products server running on ${port}`)
+);
