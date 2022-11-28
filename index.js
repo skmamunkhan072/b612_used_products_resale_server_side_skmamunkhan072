4080f4685ee3d1ccd7222e94b372e3686cb76cf2
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-11-28 20:43:50 +0600

    payment functionn add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -35,10 +36,6 @@ function verifyJWT(req, res, next) {
     req.decoded = decoded;
     next();
   });
-}
-function verifyRole(req, res, next) {
-  const email = req.query.email;
-  console.log(email);
 }
 
 async function run() {
@@ -76,11 +73,25 @@ async function run() {
       }
       res.status(403).send({ message: "forbidden access" });
     });
+
     // one user
     app.get("/user", async (req, res) => {
       const loginUserEmail = req.query.email;
       const loginUserQuery = { email: loginUserEmail };
       const result = await usersCollection.findOne(loginUserQuery);
+      res.send(result);
+    });
+    // user verify
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          sellersVerify: true,
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
 
@@ -107,28 +118,7 @@ async function run() {
     // products category create
     app.get("/all-products-category/", async (req, res) => {
       const authHeader = req?.headers?.authorization;
-      const id = req?.query?.categoryId;
-      var query = {};
-      //jwt token verify
-      if (id && authHeader) {
-        var query = { _id: ObjectId(id) };
-        if (!authHeader) {
-          return res.status(401).send("unauthorized access");
-        }
-        const token = authHeader.split(" ")[1];
-        let error = false;
-        if (!token) {
-          return res.status(403).send({ message: "forbidden access" });
-        }
-        jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-          if (err) {
-            return (error = true);
-          }
-        });
-        if (error) {
-          return res.status(403).send({ message: "forbidden access" });
-        }
-      }
+      const query = {};
       const result = await allProductsCategoryNameCollection
         .find(query)
         .toArray();
@@ -195,6 +185,27 @@ async function run() {
       const result = await bookingProductsCollection.insertOne(booking);
       res.send(result);
     });
+    // booking products
+    app.get("/my-booking-products", verifyJWT, async (req, res) => {
+      const email = req.decoded.email;
+      const role = await usersCollection.findOne({ email });
+      if (!role?.selectedRole === "user") {
+        return res.status("043").send({ message: "forbidden access" });
+      }
+      const query = { email };
+      const result = await bookingProductsCollection.find(query).toArray();
+      const products = [];
+      for (const product of result) {
+        const productId = product?.bookingProductId;
+        const dataquery = { _id: ObjectId(productId) };
+        const resultDAta = await allProductsCategoryCollection
+          .find(dataquery)
+          .toArray();
+
+        products.push(resultDAta[0]);
+      }
+      res.send(products);
+    });
 
     // AdvertisedItems function
     app.get("/advertised-items", async (req, res) => {
@@ -202,6 +213,58 @@ async function run() {
       const query = { email, advertised: true };
       const result = await allProductsCategoryCollection.find(query).toArray();
       res.send(result);
+    });
+
+    //payment function products
+    app.get("/dashboard/payment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allProductsCategoryCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/dashboard/payments", async (req, res) => {
+      const bookingData = req?.body;
+      const bookingId = bookingData?.bookingId;
+      const query = { _id: ObjectId(bookingId) };
+      const bookingQuery = { bookingProductId: bookingId };
+      const options = { upsert: true };
+      const bookingDoc = {
+        $set: {
+          transactionId: bookingData?.transactionId,
+          product: "paid",
+        },
+      };
+      const updateDoc = {
+        $set: {
+          product: "paid",
+        },
+      };
+      const booking = await bookingProductsCollection.updateOne(
+        bookingQuery,
+        bookingDoc,
+        options
+      );
+      const result = await allProductsCategoryCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+
+      req.send(result);
+    });
+    //payment function
+    app.post("/dashboard/payment", async (req, res) => {
+      const booking = req.body;
+      const price = booking.resalePrice;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
   } finally {
   }
